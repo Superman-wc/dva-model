3b3db9a35ae091ecd1698ad1ccb8e7543203dec4
superman <bainx@126.com>
2019-07-05 17:52:41 +0800

    2.0.2

--- src/Model.js
+++ src/Model.js
@@ -3,17 +3,17 @@ import restful from './restful';
 
 
 export default function Model({
-  namespace = "",
-  state = {},
-  effects = {},
-  reducers = {},
-  subscriptions = {}
-}, service = {}, caches = {}) {
+                                namespace,
+                                state = {},
+                                effects = {},
+                                reducers = {},
+                                subscriptions = {},
+                              }, service = {}, caches = {}) {
   const service_reducers = {};
   const service_effects = Object.keys(service).reduce((eff, key) => {
-    const reducer = key + 'Success';
-    eff[key] = effect(service[key], reducer, key+'Failed', caches[key]);
-    service_reducers[reducer] = restful[reducer] || ((state, action) => ({...state, [key]: action.payload }));
+    const reducer = `${key}Success`;
+    eff[key] = effect(service[key], reducer, `${key}Failed`, caches[key]);
+    service_reducers[reducer] = restful[reducer] || ((state, action) => ({...state, [key]: action.payload}));
     return eff;
   }, {});
 
@@ -22,9 +22,9 @@ export default function Model({
       if (typeof reducers[key] === 'string') {
         const _key = key.replace(/Success$/gi, '');
         m[key] = (state, action) => ({
-            ...state,
-            [_key]: reducers[key] === '' ? action.payload : action.payload[reducers[key]]
-          });
+          ...state,
+          [_key]: reducers[key] === '' ? action.payload : action.payload[reducers[key]]
+        });
       } else {
         m[key] = (state, action) => reducers[key](state, action);
       }
@@ -40,14 +40,14 @@ export default function Model({
     subscriptions,
     effects: {
       ...service_effects,
-      ...effects
+      ...effects,
     },
     reducers: {
       set(state, action) {
-        return {...state, ...action.payload };
+        return {...state, ...action.payload};
       },
       clean(state, action) {
-        return {...action.payload };
+        return {...action.payload};
       },
       ...service_reducers,
       ...successReducers
--- src/effect.js
+++ src/effect.js
@@ -6,41 +6,43 @@
  * @param cache     可选，缓存方法
  * @returns {Function}
  */
-export default function effect(api, success, fail='failed', cache=null) {
+export default function effect(api, success, fail = 'failed', cache = null) {
   return (
-    function*(action, saga) {
+    function* (action, saga) {
       if (cache) {
         const payload = cache(null, action);
         if (payload) {
           yield saga.put({
             type: action.success || success,
             payload,
-            source: action
+            source: action,
           });
         }
       }
 
-      const { result, error } = yield saga.call(api, payload);
+      const { result, error } = yield saga.call(api, action.payload);
       if (error) {
         console.error(error);
-        error.action = action; //如果可以的话，在处理完错误后重新dispatch(action);
-        if(action.reject){
+        error.action = action; // 如果可以的话，在处理完错误后重新dispatch(action);
+        if (action.reject) {
           action.reject(error);
         }
         yield saga.put({
           type: action.fail || fail,
           payload: error,
-          source: action
+          source: action,
         });
       } else {
-        cache && cache(result, action);
-        if(action.resolve){
+        if (cache) {
+          cache(result, action);
+        }
+        if (action.resolve) {
           action.resolve(result);
         }
         yield saga.put({
           type: action.fail || success,
           payload: result,
-          source: action
+          source: action,
         });
       }
     }
